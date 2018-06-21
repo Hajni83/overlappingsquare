257603dc2ada795d5c1f7d68c9a5536e33d63b77
jtar <jozsef.tar@atollgroup.eu>
2018-06-21 23:14:25 +0200

    introduce

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+    var canvas=document.getElementById('MyCanvas');
+    var context=canvas.getContext('2d');
+    context.fillStyle='rgb(255, 0, 0';
+    context.fillRect(80,80,80,80);
+    context.fillStyle='red';
+    context.fillRect(120,120,80,80);
+  });
