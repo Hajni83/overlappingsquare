3af11c36869d44e609bfa7ce592ba4ac5c7a114c
jtar <jozsef.tar@atollgroup.eu>
2018-06-21 23:37:35 +0200

    finished

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(function() {
     var canvas=document.getElementById('MyCanvas');
     var context=canvas.getContext('2d');
-    context.fillStyle='rgb(255, 0, 0';
-    context.fillRect(80,80,80,80);
-    context.fillStyle='red';
-    context.fillRect(120,120,80,80);
+    var rectWidth=80;
+    var rectHeight=80;
+    var square1=80;
+    var square2=120;
+    context.fillStyle='rgba(255, 0, 0, 0.6)';
+    context.fillRect(square1,square1,rectWidth,rectHeight);
+    context.fillStyle='rgba(0,0,255,0.3)';
+    context.fillRect(square2,square2,rectWidth,rectHeight);
   });
